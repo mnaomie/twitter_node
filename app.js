eb0836e19851c54aa0511db8bd68d39eb5abe6a4
naomie marco <naomiemarco7@gmail.com>
2023-02-13 10:13:26 +0100

    feat : mise en place de la topbar

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require ('path');
+const index = require('./routes/')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('short'));
+
+app.use(index);
+
+app.listen(PORT);
---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000);
