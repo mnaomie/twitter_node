d578e789b2ec72bea160dd98c3ff00e89c8b048b
naomie marco <naomiemarco7@gmail.com>
2023-03-16 11:02:13 +0100

    feat : comment route added

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const tweetRoute = require('./tweet.routes');
 const userRoute = require('./user.routes')
 const { tweetlist } = require('../controllers/tweet.controller');
 const { route } = require('./tweet.routes');
+const commentRoute = require('./Comment.routes')
 const authRoute = require('./auth.routes');
 const { ensureAuthenticated } = require('../config/security.config');
 
 router.use('/tweet', ensureAuthenticated, tweetRoute);
 router.use('/user', userRoute)
 router.use('/auth', authRoute)
+router.use('/comment', ensureAuthenticated, commentRoute)
 
 router.get('/', tweetlist)
 
